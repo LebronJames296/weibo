98e46e080968b1453d97a844dc944c479295b24d
James <296466865@qq.com>
2019-04-14 01:30:48 +0000

    管理员可删除用户

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -19,6 +19,7 @@ class UsersTableSeeder extends Seeder
         $user =User::find(1);
         $user->name='James';
         $user->email='296466865@qq.com';
+        $user->is_admin=true;
         $user->save();
     }
 }
