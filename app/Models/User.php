c95783fb1a2effdd3415a96e47943f8386b35d2e
James <296466865@qq.com>
2019-04-16 11:46:56 +0000

    微博数据模型

--- app/Models/User.php
+++ app/Models/User.php
@@ -53,4 +53,10 @@ class User extends Authenticatable
             $user->activation_token =str_random(30);
         });
     }
+
+    public function statuses()
+    {
+        return $this->hasMany(Status::class);
+
+    }
 }
